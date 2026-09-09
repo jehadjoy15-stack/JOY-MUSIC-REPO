34dbda849f2c1255424254ee853f56c952f784e8
jehadjoy15-stack <jehadjoy69@gmail.com>
2026-09-10 04:16:06 +0600

    chore(release): bump version to 13.6.4 (153)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,8 +107,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 152
-        versionName = "13.6.3"
+        versionCode = 153
+        versionName = "13.6.4"
         val baseVersionName = requireNotNull(versionName)
         buildConfigField("String", "BASE_VERSION_NAME", "\"$baseVersionName\"")
         buildCommit?.let { versionName = "$baseVersionName+$it" }
